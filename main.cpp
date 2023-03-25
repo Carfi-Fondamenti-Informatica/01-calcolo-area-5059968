37edea8875de792aed90ef3ca159db6560d97a5a
5059968 <128074987+5059968@users.noreply.github.com>
2023-03-25 17:19:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+
+float a=0;
+float b=0;
+float c=0;
+float area_triangolo=0;
+float area_quadrato=0;
+float area_rettangolo=0;
+float area_trapezio=0;
 
 cin >> a >> b >> c;
+area_triangolo=a*b/2;
+area_quadrato=a*a;
+area_rettangolo=a*b;
+area_trapezio=(a+b)*c/2;
+cout<<area_triangolo<<endl;
+cout<<area_quadrato<<endl;
+cout<<area_rettangolo<<endl;
+cout<<area_trapezio<<endl;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+return 0;
 
 
 }
